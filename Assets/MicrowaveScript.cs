53d677b34e11a65b9ca3686880df57464e041da9
up202103342 <emts2002@gmail.com>
2025-03-15 19:44:04 +0000

    fixed cleaning lady mechanic

--- Assets/MicrowaveScript.cs
+++ Assets/MicrowaveScript.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class Microwave : MonoBehaviour
 {
+    private bool isPlayerNear = false;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) // Change "U" to whatever key you want
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerNear) // Change "U" to whatever key you want
         {
             CollectibleManager.Instance.UseUnlockableItem();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            Debug.Log("Press 'E' to collect the item.");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
         }
     }
 }
